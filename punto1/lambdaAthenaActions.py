0e673153a20369ecd282079369caf566126cab34
JAIR DUVAN AYALA DUARTE <jair-07020@hotmail.com>
2021-03-13 18:46:48 -0500

    Se adjunta la tarea de Athena en el mismo script de lambda

--- punto1/lambdaAthenaActions.py
+++ punto1/lambdaAthenaActions.py
@@ -39,25 +39,52 @@ cementos_argos = 'CMTOY'
 grupo_aval = 'AVAL'
 avianca = 'AVHOQ'
 ecopetrol = 'EC'
+### assignment of data for each action
 stocks_cementos_argos = DataReader(cementos_argos,'yahoo',start=inicioMal)
-stocks_cementos_argos = stocks_cementos_argos.iloc[-3:-1,:]
+stocks_cementos_argos = stocks_cementos_argos.iloc[-2:-1,:]
 stocks_grupo_aval = DataReader(grupo_aval,'yahoo',start=inicio)
 stocks_avianca = DataReader(avianca,'yahoo',start=inicio)
 stocks_ecopetrol = DataReader(ecopetrol,'yahoo',start=inicio)
+### assignment of data in an array
 stocks_actions=[stocks_avianca,stocks_cementos_argos,stocks_ecopetrol,stocks_avianca]
 ### New Bucket
 newBucket='parcialbigdata1'
 
+
+client = boto3.client('athena', region_name='us-east-1')
+
+
 for i in range(4):
-    upload_path= newBucket+'/stocks/company='+actions[i]+'/year='+str(year_actions)+'/month='+str(month_actions)+'/day='+str(day_actions)+'/'+actions[i]+'.csv'
+    upload_path = newBucket+'/stocks/company='+actions[i]+'/year='+str(year_actions)+'/month='+str(month_actions)+'/day='+str(day_actions)+'/'+actions[i]+'.csv'
     ### Upload file
     stocks_actions[i].to_csv(actions[i]+'.csv')
+    ### Upload files
     s3.upload_file(actions[i]+'.csv', newBucket, upload_path)
 
+    ### creation of parameters for Athena
+    params = {
+        'region': 'us-east-1',
+        'database': 'punto1',
+        'bucket': 'parcialbigdata1',
+        'path': '/parcialbigdata1/stocks/',
+        'query': 'alter table actions add partition(company="{}",year="{}",month="{}",day="{}");'.format(actions[i],year_actions,month_actions,day_actions)
+    }
 
+    ### creation of queryfor Athena
+    response_query_execution_id = client.start_query_execution(
+        QueryString = params['query'],
+        QueryExecutionContext = {
+            'Database' : params['database']
+        },
+        ResultConfiguration = {
+            'OutputLocation': 's3://' + params['bucket'] + '/' + params['path']
+        }
+    
+    )
+    
+    ### query response details
+    response_get_query_details = client.get_query_execution(
+        QueryExecutionId = response_query_execution_id['QueryExecutionId']
+    )
 
-
-
-
-
-
+    print("Result Data: "+actions[i])

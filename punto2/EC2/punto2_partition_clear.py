c8d8a0237683dd15efb407489fe06111ddd388a6
Lohanna <lohanna.aguirre@correo.usa.edu.co>
2021-03-14 19:27:19 -0500

    Punto2 codigo  de particion de athena limpio

---
+++ punto2/EC2/punto2_partition_clear.py
@@ -0,0 +1,50 @@
+import json
+from pandas_datareader import DataReader
+import datetime as dt
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+import sys
+import uuid
+from urllib.parse import unquote_plus
+import boto3
+
+s3 = boto3.client('s3')
+   
+today = dt.datetime.today()
+day_actions = today.day
+month_actions = today.month
+year_actions = today.year
+
+newBucket='parcial1-bigdata2021'
+
+client = boto3.client('athena', region_name='us-east-1')
+
+paper=['eltiempo','publimetro']
+
+
+for i in range(len(paper)):
+    params = {
+        'region': 'us-east-1',
+        'database': 'noticia',
+        'bucket': 'parcial1-bigdata2021',
+        'path': 'punto2/headlines/raw',
+        'query': 'alter table periodicos add partition(periodico="{}",year="{}",month="{}",day="{}");'.format(paper[i],year_actions,month_actions,day_actions)
+    }
+    response_query_execution_id = client.start_query_execution(
+        QueryString = params['query'],
+        QueryExecutionContext = {
+            'Database' : params['database']
+        },
+        ResultConfiguration = {
+            'OutputLocation': 's3://' + params['bucket'] + '/' + params['path']
+        }
+    
+    )
+    response_get_query_details = client.get_query_execution(
+        QueryExecutionId = response_query_execution_id['QueryExecutionId']
+    )
+
+    print("Result Data: "+paper[i])
+
